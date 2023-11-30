89eb11048d64a46a256c45403a5a29b22ab37a3b
Peter Mueller <peter.kh.mueller@gmail.com>
2023-11-29 21:53:55 -0500

    fix iex config bug

--- iex.exs
+++ iex.exs
@@ -3,23 +3,22 @@
 IO.puts IO.ANSI.magenta_background() <> IO.ANSI.white() <> " ❄❄❄ All things serve the BEAM ❄❄❄ " <> IO.ANSI.reset
 Application.put_env(:elixir, :ansi_enabled, true)
 IEx.configure(
- colors: [
-   eval_result: [:green, :bright] ,
-   eval_error: [[:red,:bright,"Bug Bug ..!!"]],
-   eval_info: [:yellow, :bright ],
- ],
- default_prompt: [
-   "\e[G",    # ANSI CHA, move cursor to column 1
+  colors: [
+    eval_result: [:green, :bright] ,
+    eval_error: [[:red,:bright,"Bug Bug ..!!"]],
+    eval_info: [:yellow, :bright ],
+  ],
+  default_prompt: [
     "%prefix",:white,"|",
-     :blue,
-     "%counter",
-     :white,
-     "|",
+      :blue,
+      "%counter",
+      :white,
+      "|",
     :red,
     "▶" ,         # plain string
     :white,
     "▶▶"  ,       # plain string
       # ❤ ❤-»" ,  # plain string
     :reset
-  ] |> IO.ANSI.format |> IO.chardata_to_string
+  ] |> IO.ANSI.format() |> IO.chardata_to_string
 )
