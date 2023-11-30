907eafd9ddb18b289fdc8f9cbf298cde8bd99f92
Peter Mueller <peter.kh.mueller@gmail.com>
2023-11-29 21:59:25 -0500

    disable p10k while testing starship; tmux changes

--- zsh/custom/plugins/unorganized_misc/unorganized_misc.plugin.zsh
+++ zsh/custom/plugins/unorganized_misc/unorganized_misc.plugin.zsh
@@ -4,8 +4,21 @@
 # tmux Plugin - ofirgall/tmux-window-name
 # Sets tmux window on `cd`
 tmux-window-name() {
-	($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
+	if [[ $TERM_PROGRAM == "tmux" ]]; then
+		($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
+	fi
 }
+
 
 add-zsh-hook chpwd tmux-window-name
 # /end ofirgall/tmux-window-name
+
+
+env-export() {
+	local conf_file=$1
+
+	set -o allexport && source ${conf_file} && set +o allexport
+}
+
+
+eval "$(starship init zsh)"
