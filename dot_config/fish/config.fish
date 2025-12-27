7999ec03adac74ba4d8c84223d9939a3836d9ff4
Peter Mueller <6015288+petermueller@users.noreply.github.com>
2025-12-26 22:58:41 -0500

    add tide plugin over starship

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,8 +3,7 @@
 # Disable greeting
 set -g fish_greeting
 
-# Initialize Starship prompt
-starship init fish | source
+eval "$(/opt/homebrew/bin/brew shellenv)" | source
 
 # Fisher package manager will be installed on first run
 # Run: curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
